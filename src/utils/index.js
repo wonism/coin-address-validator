05559b71c2206f90894e706f3001980c726b1fa0
wonism <jaewonism57@gmail.com>
2018-07-09 20:12:41 +0900

    v1.0.4 - add polyfill

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,3 +1,26 @@
+/**
+ * polyfill from MDN
+ */
+/* eslint-disable */
+if (!String.prototype.padStart) {
+  String.prototype.padStart = function padStart(targetLength,padString) {
+    targetLength = targetLength>>0; //truncate if number or convert non-number to 0;
+    padString = String((typeof padString !== 'undefined' ? padString : ' '));
+    if (this.length > targetLength) {
+      return String(this);
+    }
+    else {
+      targetLength = targetLength-this.length;
+
+      if (targetLength > padString.length) {
+        padString += padString.repeat(targetLength/padString.length); //append to original to ensure we are longer than needed
+      }
+      return padString.slice(0,targetLength) + String(this);
+    }
+  };
+}
+/* eslint-enable */
+
 const numberToHex = number => Math.round(number).toString(16).padStart(2, '0');
 
 export const toHex = arr =>
